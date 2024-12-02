6fd2c41467380a99e18a9f0c1ffcac63d383bc73
Gulshan Yadav <gulshanyadav0029@gmail.com>
2024-12-02 17:58:16 +0530

    final change in system

---
+++ app.js
@@ -0,0 +1,17 @@
+// let name=prompt("Enter your name");
+// let profile=document.querySelector("#profile");
+// // profile.innerText=name;
+// console.log(profile);
+
+
+// let imgs=document.querySelectorAll(".image");
+
+
+// // console.log(img);
+// for (i of imgs){
+//     i.addEventListener("click",()=>{
+//         console.log("clicked");
+//         console.log("don't click");
+//         alert("Hello user");
+//     })
+// }
